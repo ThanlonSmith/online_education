51f860e4688ddd62680346865b9895e3a49a32a2
erics1996 <erics1996@yeah.net>
2020-10-13 04:50:10 +0800

    Completed related functions of course detail.

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from .views import course_list
+from django.urls import path, re_path
+from .views import course_list, course_detail
 
 urlpatterns = [
     path('list/', course_list, name='course_list'),
+    re_path('^detail/(\d+)/$', course_detail, name='course_detail'),
 ]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import CourseInfo
+from ..operations.models import UserLove
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
@@ -43,3 +44,30 @@ def course_list(request):
             'num_pages': num_pages,
             'sort': sort
         })
+
+
+# 课程详情
+def course_detail(request, course_id):
+    if request.method == "GET":
+        if course_id:
+            current_course = CourseInfo.objects.filter(id=course_id)
+            if current_course.exists():
+                current_course = current_course.first()
+                relate_course = CourseInfo.objects.filter(category=current_course.category).exclude(
+                    id=course_id)[:2]  # <QuerySet []>
+                # 判断机构和课程是否被当前用户收藏
+                print(request.user.is_authenticated)  # True or False
+                if request.user.is_authenticated:
+                    org_love, course_love = False, False
+                    if UserLove.objects.filter(love_man=request.user, love_id=current_course.org_info.id, love_type=1,
+                                               love_status=True).exists():
+                        org_love = True
+                    if UserLove.objects.filter(love_man=request.user, love_id=course_id, love_type=2,
+                                               love_status=True).exists():
+                        course_love = True
+                    return render(request, 'courses/course_detail.html', {
+                        'current_course': current_course,
+                        'relate_course': relate_course,
+                        'org_love': org_love,
+                        'course_love': course_love
+                    })
